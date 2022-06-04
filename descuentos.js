e176eaede2a9151268959b8872f615fcae31fad5
rocker8901 <jotafer@gmail.com>
2022-06-04 16:27:50 -0500

    Se cambio ingresar el valor del descuento, por ingresar un nombre de cupon de descuento

--- descuentos.js
+++ descuentos.js
@@ -15,15 +15,41 @@ function obtenerPrecio(){
     const inputP = document.getElementById("inputP");
     const valueP = inputP.value;
 
-    const inputD = document.getElementById("inputD");
-    const valueD = inputD.value;
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const valorFinalCompra = precioConDescuento(valueP, valueD);
+    const coupons = [
+        {
+            name: "Kilo",
+            discount: 10
+        },
+        {
+            name: "Mega",
+            discount: 20            
+        },
+        {
+            name: "Giga",
+            discount: 30
+        }
+    ];
 
-    console.log(valorFinalCompra);
+    const isCouponValueValid = function(coupons){
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio final es " + valorFinalCompra;
-    //alert(valorFinalCompra);
+        return coupons.name === couponValue;        
+    }
+ 
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon){
+        alert ("El cupon " + couponValue + " no es valido");
+    }
+    
+    else {
+
+        const descuento = userCoupon.discount;
+        const valorFinalCompra = precioConDescuento(valueP, descuento);
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio final es " + valorFinalCompra;
+    }
 }
 
