01bdf993bfb56ae8c97de426b31265e2a3ac080c
rocker8901 <jotafer@gmail.com>
2022-06-05 00:28:48 -0500

    se formateo el codigo de la mediana con prettier

--- mediana.js
+++ mediana.js
@@ -1,29 +1,26 @@
-
 const lista = [236, 25, 3, 458, 6];
 
-function ordenarLista(lista){
-
-    lista.sort(function(a,b){return a - b});
-    return lista;
+function ordenarLista(lista) {
+  lista.sort(function (a, b) {
+    return a - b;
+  });
+  return lista;
 }
 
 //const sizeLista = listaOrd.length;
 ordenarLista(lista);
 
-function esPar(dato){
-
-    if (dato % 2 === 0) {
-        return true;
-    }
-    else{
-        return false;
-    }
+function esPar(dato) {
+  if (dato % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function promedio (num1, num2){
-
-    const resul = (num1 +num2) / 2;
-    return resul;
+function promedio(num1, num2) {
+  const resul = (num1 + num2) / 2;
+  return resul;
 }
 
 let mediana;
@@ -31,14 +28,8 @@ const mitadlista = parseInt(lista.length / 2);
 const numero1 = lista[mitadlista - 1];
 const numero2 = lista[mitadlista];
 
-if (esPar(lista.length)){    
-
-    mediana = promedio(numero1, numero2);
-    console.log()
-
-}
-else {
-
-    mediana = lista[mitadlista];
-
+if (esPar(lista.length)) {
+  mediana = promedio(numero1, numero2);
+} else {
+  mediana = lista[mitadlista];
 }
