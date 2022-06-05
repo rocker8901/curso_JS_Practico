7a253653d5389859aa7023c98cb9f5788038daa1
rocker8901 <jotafer@gmail.com>
2022-06-05 00:25:00 -0500

    se añadio el codifo para sacra la mediana

---
+++ mediana.js
@@ -0,0 +1,44 @@
+
+const lista = [236, 25, 3, 458, 6];
+
+function ordenarLista(lista){
+
+    lista.sort(function(a,b){return a - b});
+    return lista;
+}
+
+//const sizeLista = listaOrd.length;
+ordenarLista(lista);
+
+function esPar(dato){
+
+    if (dato % 2 === 0) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function promedio (num1, num2){
+
+    const resul = (num1 +num2) / 2;
+    return resul;
+}
+
+let mediana;
+const mitadlista = parseInt(lista.length / 2);
+const numero1 = lista[mitadlista - 1];
+const numero2 = lista[mitadlista];
+
+if (esPar(lista.length)){    
+
+    mediana = promedio(numero1, numero2);
+    console.log()
+
+}
+else {
+
+    mediana = lista[mitadlista];
+
+}
---
+++ promedio.js
@@ -0,0 +1,21 @@
+function calcularPromedio(lista){
+  /*  let sumaLista = 0;
+
+    for (let i=0; i< lista.length; i ++ ){
+        sumaLista = sumaLista + lista[i];
+    }*/
+
+const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento ){
+        return valorAcumulado + nuevoElemento;
+
+    }
+);
+const promedio = sumaLista / lista.length;
+
+return promedio;
+
+}
+
+
+
